0f4d82cc60bad04f62ec9af8827d505146b3b184
jkomyno <skiabo97@gmail.com>
2020-01-08 16:48:19 +0100

    Created main parser entrypoint

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,40 @@
 module Main where
 
 import Lib
+import System.IO
+import Data.Maybe
 
-main :: IO ()
-main = someFunc
+defaultFileName = "example.core"
+
+readF :: String -> IO String
+readF filename = do
+    inh <- openFile filename ReadMode
+    prog <- readloop inh
+    hClose inh
+    return prog
+
+main :: IO (Program Name)
+main = do
+    putStr $ "Insert source filename [" ++ defaultFileName ++ "]: "
+    hFlush stdout
+    fileName <- getLine
+    -- if fileName is empty, use defaultFileName
+    program <- readF $ fromMaybe defaultFileName $ Just fileName
+    let result = comp $ parse parseProgr program
+    print result
+    return result
+
+comp :: [(CoreProgram, Name)] -> CoreProgram
+comp []       = error "no parse"
+comp [(e,[])] = e
+comp [(_,a)]  = error ("doesn't use all input" ++ a)
+
+readloop :: Handle -> IO String
+readloop inh = do
+    ineof <- hIsEOF inh
+    if ineof
+      then return []
+      else do
+        x <- hGetLine inh
+        xs <- readloop inh
+        return (x ++ xs)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,14 @@
-module Lib where
+module Lib (
+    CoreProgram,
+    Program,
+    Name,
+    Expr(..),
+    parse,
+    parseExpr,
+    parseProgr,
+) where
 
 import Core
+import Parse (parse)
+import ParseExpr (parseExpr)
+import ParseProgr (parseProgr)
